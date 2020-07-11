f2a963261f63c36d93f9c2ef8e7a738ba25b13b3
Jake Morrison <jake@cogini.com>
2020-07-11 11:39:31 +0800

    Pull at top

--- ecs/build.sh
+++ ecs/build.sh
@@ -60,8 +60,4 @@ esac
 echo "CACHE_FROM: ${CACHE_FROM}"
 echo "CACHE_TO: ${CACHE_TO}"
 
-docker pull elixir:1.10.4-alpine
-docker pull node:14.4-stretch
-docker pull alpine:3.12
-
 docker buildx build $CACHE_FROM $CACHE_TO --target $TARGET $TAGS -f $DOCKERFILE $PROGRESS $OUTPUT "."
