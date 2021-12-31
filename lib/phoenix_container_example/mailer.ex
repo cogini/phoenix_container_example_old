4c0d4cc4cc737ba86d9f3bb9cd33d4aff6e44621
Jake Morrison <jake@cogini.com>
2021-12-31 15:45:29 +0800

    Add @moduledoc for credo

---
+++ lib/phoenix_container_example/mailer.ex
@@ -0,0 +1,4 @@
+defmodule PhoenixContainerExample.Mailer do
+  @moduledoc false
+  use Swoosh.Mailer, otp_app: :phoenix_container_example
+end
--- lib/phoenix_container_example_web/telemetry.ex
+++ lib/phoenix_container_example_web/telemetry.ex
@@ -33,11 +33,27 @@ defmodule PhoenixContainerExampleWeb.Telemetry do
       ),
 
       # Database Metrics
-      summary("phoenix_container_example.repo.query.total_time", unit: {:native, :millisecond}),
-      summary("phoenix_container_example.repo.query.decode_time", unit: {:native, :millisecond}),
-      summary("phoenix_container_example.repo.query.query_time", unit: {:native, :millisecond}),
-      summary("phoenix_container_example.repo.query.queue_time", unit: {:native, :millisecond}),
-      summary("phoenix_container_example.repo.query.idle_time", unit: {:native, :millisecond}),
+      summary("phoenix_container_example.repo.query.total_time",
+        unit: {:native, :millisecond},
+        description: "The sum of the other measurements"
+      ),
+      summary("phoenix_container_example.repo.query.decode_time",
+        unit: {:native, :millisecond},
+        description: "The time spent decoding the data received from the database"
+      ),
+      summary("phoenix_container_example.repo.query.query_time",
+        unit: {:native, :millisecond},
+        description: "The time spent executing the query"
+      ),
+      summary("phoenix_container_example.repo.query.queue_time",
+        unit: {:native, :millisecond},
+        description: "The time spent waiting for a database connection"
+      ),
+      summary("phoenix_container_example.repo.query.idle_time",
+        unit: {:native, :millisecond},
+        description:
+          "The time the connection spent waiting before being checked out for the query"
+      ),
 
       # VM Metrics
       summary("vm.memory.total", unit: {:byte, :kilobyte}),
