0691a65aee4bd45b3093f7f1856893bdaad2de3d
Jake Morrison <jake@cogini.com>
2022-02-09 18:37:37 +0800

    mix format

--- test/phoenix_container_example_web/views/error_view_test.exs
+++ test/phoenix_container_example_web/views/error_view_test.exs
@@ -9,6 +9,7 @@ defmodule PhoenixContainerExampleWeb.ErrorViewTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(PhoenixContainerExampleWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(PhoenixContainerExampleWeb.ErrorView, "500.html", []) ==
+             "Internal Server Error"
   end
 end
