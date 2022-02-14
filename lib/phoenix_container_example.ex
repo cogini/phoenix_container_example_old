59075086193cfc565b8e4d5b9faabae986f834b6
Jake Morrison <jake@cogini.com>
2022-02-14 19:30:19 +0800

    Add OpenTelemetry

--- lib/phoenix_container_example.ex
+++ lib/phoenix_container_example.ex
@@ -6,6 +6,7 @@ defmodule PhoenixContainerExample do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  require OpenTelemetry.Tracer, as: Tracer
 
   def hello_otel do
     Tracer.with_span "operation" do
