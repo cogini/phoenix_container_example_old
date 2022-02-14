f7b83bbe3fecda041cfe6792ca180f7df79cd75b
Jake Morrison <jake.morrison@therealreal.com>
2022-02-14 19:07:33 +0800

    Add sample OTEL trace

--- lib/phoenix_container_example.ex
+++ lib/phoenix_container_example.ex
@@ -6,4 +6,16 @@ defmodule PhoenixContainerExample do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def hello_otel do
+    Tracer.with_span "operation" do
+      Tracer.add_event("Nice operation!", [{"bogons", 100}])
+      Tracer.set_attributes([{:another_key, "yes"}])
+
+      Tracer.with_span "Sub operation..." do
+        Tracer.set_attributes([{:lemons_key, "five"}])
+        Tracer.add_event("Sub span event!", [])
+      end
+    end
+  end
 end
