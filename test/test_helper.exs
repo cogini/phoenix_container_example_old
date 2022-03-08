07efcaa2fd136dd4adf83232ef3b617656c076fc
Jake Morrison <jake@cogini.com>
2022-03-08 20:06:15 +0800

    mix format

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,3 @@
-ExUnit.configure formatters: [JUnitFormatter, ExUnit.CLIFormatter]
+ExUnit.configure(formatters: [JUnitFormatter, ExUnit.CLIFormatter])
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(PhoenixContainerExample.Repo, :manual)
