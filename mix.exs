d8eca7390cab66fd9a0db437f407d616a321d5d2
Jake Morrison <jake@cogini.com>
2023-09-17 15:11:54 -0500

    Upgrade libraries

--- mix.exs
+++ mix.exs
@@ -96,7 +96,7 @@ defmodule PhoenixContainerExample.MixProject do
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_dashboard, "~> 0.6"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17.5"},
+      {:phoenix_live_view, "~> 0.17.7"},
       {:plug_cowboy, "~> 2.5"},
       {:postgrex, ">= 0.0.0"},
       {:sobelow, "~> 0.11", only: [:dev, :test], runtime: false},
