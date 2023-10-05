1b7fd4642a166e626639995b135209f361767105
Jake Morrison <jake@cogini.com>
2023-10-05 12:09:54 -0500

    Add tailwind

--- config/config.exs
+++ config/config.exs
@@ -47,6 +47,18 @@ config :esbuild,
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
 
+# Configure tailwind (the version is required)
+config :tailwind,
+  version: "3.3.2",
+  default: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../assets", __DIR__)
+  ]
+
 config :logger,
   level: :info
 
