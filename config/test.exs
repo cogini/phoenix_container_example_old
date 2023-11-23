6ddccf146d5176616ade8217027f2a2e22a5394c
Jake Morrison <jake@cogini.com>
2023-11-22 18:10:33 -0600

    Be quiet about test output

--- config/test.exs
+++ config/test.exs
@@ -44,7 +44,7 @@ config :junit_formatter,
   report_dir: "#{Mix.Project.build_path()}/junit-reports",
   # report_dir: "junit-reports",
   automatic_create_dir?: true,
-  print_report_file: true,
+  # print_report_file: true,
   # prepend_project_name?: true,
   include_filename?: true,
   include_file_line?: true
